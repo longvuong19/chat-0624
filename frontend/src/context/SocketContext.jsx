9a4b6056b22f05f4b9bf2bb0e77ed9dfcace1355
longvuong19 <longvuong1920@gmail.com>
2024-07-18 00:00:56 +0700

    update Socket URL

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:9000", {
+      const socket = io("https://chat-0624.onrender.com", {
         query: {
           userId: authUser._id,
         },
