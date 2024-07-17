6bec6b90f74d71aefa1543c8111d7d6cbcef4c01
longvuong19 <longvuong1920@gmail.com>
2024-07-17 23:43:55 +0700

    add logo and web name

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,12 +11,20 @@ import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 9000;
 dotenv.config();
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongo();
